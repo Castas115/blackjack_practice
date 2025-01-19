d19828a3ef81b59b7e89eb6bf36baca87d2fd6a7
jon <joncastas@gmail.com>
2025-01-19 18:50:47 +0100

    dealer hand done

--- game/game.go
+++ game/game.go
@@ -44,8 +44,16 @@ func (game *Game) Deal(hand *Hand) {
 }
 
 func (game *Game) ResolveRoundOutcome() {
+	game.dealerDraws()
 	for i := range game.Players {
 		game.Players[i].Wager = game.BetSize
 		game.Players[i].ResolveRoundOutcome(game.DealerHand)
+	}
+}
+
+func (game *Game) dealerDraws() {
+	if (game.DealerHand.Count() < 17) {
+		game.Deal(&game.DealerHand)
+		game.dealerDraws()
 	}
 }
